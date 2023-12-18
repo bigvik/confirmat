d0d2585167cefe5ad0f562f123075418e0fa6640
bigvik <bigvikt@gmail.com>
2023-12-18 23:39:33 +0600

    Make model and little bit refactoring

--- main.py
+++ main.py
@@ -5,24 +5,37 @@ import qrcode
 from docx import Document
 from docx.shared import Pt
 
-async def fetch(url):
+async def fetch(url:str) -> str:
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
             return await response.text()
         
-async def parse_catalog():
+async def parse_catalog() -> list:
     html = await fetch('https://www.boyard.biz/catalog')
     parser = HTMLParser(html)
     links = [a.attrs['href'] for a in parser.css('.product-card')]
     return links
 
-async def parse_links(link):
+async def parse_links(link:str) -> str:
     html = await fetch(link)
     parser = HTMLParser(html)
     links = {}
     for a in parser.css('.bd-product__link'):
         links.update({a.attributes['title']: a.attributes['href']})
     return links
+
+async def make_qr(text: str, name:str) -> None:
+    qr = qrcode.make(text)
+    name = text.split('/')[-1:][0]
+    qr.save('qr/'+name+'.png')
+
+async def save_data(data):
+    from model import DataSaver
+    data_saver = DataSaver('data.db')
+    data_saver.create_table('qr', ['name TEXT', 'url TEXT'])
+    for item in data:
+        data_saver.insert_data('qr', item)
+    data_saver.close_connection()
 
 async def main():
     catalog = await parse_catalog()
@@ -31,9 +44,8 @@ async def main():
     for link in catalog:
         links = await parse_links(link)
         for k in links:
-            qr = qrcode.make(links[k])
-            name = links[k].split('/')[-1:][0]
-            qr.save('qr/'+name+'.png')
+            name = links[k].split('/')[-1:][0][:-5]
+            await make_qr(links[k], name)
             doc.add_paragraph(k)
             doc.add_picture('qr/'+name+'.png', width=Pt(100))
             print(name+'.png')
---
+++ model.py
@@ -0,0 +1,23 @@
+import sqlite3
+
+class DataSaver:
+    def __init__(self, db_name):
+        self.conn = sqlite3.connect(db_name)
+        self.cursor = self.conn.cursor()
+    
+    def create_table(self, table_name, columns):
+        query = f"CREATE TABLE IF NOT EXISTS {table_name} ({', '.join(columns)})"
+        self.cursor.execute(query)
+    
+    def insert_data(self, table_name, data):
+        placeholders = ', '.join(['?' for _ in data])
+        query = f"INSERT INTO {table_name} VALUES ({placeholders})"
+        self.cursor.execute(query, data)
+        self.conn.commit()
+    
+    def close_connection(self):
+        self.cursor.close()
+        self.conn.close()
+
+if __name__ == '__main__':
+    pass
